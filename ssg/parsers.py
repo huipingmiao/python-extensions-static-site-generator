5c3152c1e5a536d226a3c9f9c6beaa1bd87ed309
Huiping Miao <Huiping.Miao@sas.com>
2023-05-02 13:27:50 -0400

    module 4

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -52,6 +52,7 @@ class MarkdownParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+        hooks.event("written")   
 
 
 class ReStructuredTextParser(Parser):
@@ -60,7 +61,9 @@ class ReStructuredTextParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(path, dest, html["html_body"])
+        filtered = hooks.filter("generate_menu", html["html_body"], self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+        hooks.event("written")
